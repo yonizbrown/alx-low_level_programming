80e0f380078d9f8a91c250646b582a6e72b7731b
yonas zeleke <yoniz0915@gmail.com>
2021-09-10 13:25:15 -0700

    another coding on pointer, string and arrays.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,7 @@ int n;
 int a[5];
 int *p;
 
-a[2] = 1024;
+1024 = a[2];
 p = &n;
 /*
 * write your line of code here...
@@ -16,6 +16,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
+p[5] = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
