9d850aea13f9b062dfc82c5b2632f7b18c1d8912
yonas zeleke <yoniz0915@gmail.com>
2021-09-10 13:31:20 -0700

    another coding on pointer, string and arrays.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,6 +13,7 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+
 while (*(n1 + c1) != '\0')
 c1++;
 while (*(n2 + c2) != '\0')
@@ -24,7 +25,7 @@ bg = c2;
 if (size_r <= bg + 1)
 return (0);
 r[bg + 1] = '\0';
-c1--, c2--, size_r--;
+c1--,c2--,size_r--;
 dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 while (bg >= 0)
 {
@@ -37,11 +38,11 @@ if (op > 0)
 *(r + bg) = (op % 10) + 48;
 else
 *(r + bg) = '0';
-if (c1 > 0)
+if(c1 > 0)
 c1--, dr1 = *(n1 + c1) - 48;
 else
 dr1 = 0;
-if (c2 > 0)
+if(c2 > 0)
 c2--, dr2 = *(n2 + c2) - 48;
 else
 dr2 = 0;
