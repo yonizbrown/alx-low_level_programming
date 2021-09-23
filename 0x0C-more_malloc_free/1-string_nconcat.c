76e239813ece2ced1d642dc9b8058546c70c6ba7
yonas zeleke <yoniz0915@gmail.com>
2021-09-23 09:57:12 -0700

    new coding on more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,9 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-;
+for (ls1 = 0; s1[ls1] != '\0'; ls1++);
 
-for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-;
+for (ls2 = 0; s2[ls2] != '\0'; ls2++);
 
 if (n > ls2)
 n = ls2;
