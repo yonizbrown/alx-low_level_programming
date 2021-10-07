4dc5d081dd9460373cf4166da9af525153f23e14
yonas zeleke <yoniz0915@gmail.com>
2021-10-07 01:05:42 -0700

    code on Singly_link

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .data
-	msg db "Hello, Holberton",10
+msg db "Hello, Holberton",10
 
-	section .text
-	global main
+section .text
+global main
 
 main:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 17
-	syscall
+mov rax, 1
+mov rdi, 1
+mov rsi, msg
+mov rdx, 17
+syscall
 
-	mov rax, 60
-	mov rdi, 0
-	syscall
+mov rax, 60
+mov rdi, 0
+syscall
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+<h1> Newer code on C.</h1>
